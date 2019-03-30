62fbd367715ee34d958fb0f0261b52d49110febc
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-03-30 10:23:44 -0700

    Populating user for dahsboard

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -2,11 +2,19 @@ const express     = require('express');
 const router      = express.Router();
 const mongoose    = require('mongoose');
 const Article 	      = mongoose.model('Article');
+const User            = mongoose.model('User');
 
 router.get('/', (req, res, next) => {
-  Article.find({})
-    .then(articles => res.render('dashboard', { articles }))
-    .catch(next);
+  User.findById(req.cookies.user)
+  .populate('card.article')
+  .then((user) => {
+    Article.find({})
+      .then(articles => {
+        console.log(user.card.map(console.log));
+        res.render('dashboard', { articles, user })
+      })
+      .catch(next);
+  });
 });
 
 router.get('/admin', (req, res, next) => {
