1fc32d65db36c6ba96498f8f11f8b3cab187f995
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-03-30 10:24:15 -0700

    Added card actions

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,15 +3,67 @@ const router      = express.Router();
 const mongoose    = require('mongoose');
 const User 	      = mongoose.model('User');
 
+const ObjectId = mongoose.Types.ObjectId;
+
 router.get('/register', (req, res, next) => {
   res.render('register');
 });
-
 
 router.post('/register', (req, res, next) => {
   const user = new User(req.body);
   user.save()
     .then(() => res.redirect('/dashboard'))
+    .catch(next);
+});
+
+router.post('/card', (req, res, next) => {
+  const articleId = req.body.id;
+  const userId = req.cookies.user;
+
+  console.log(userId);
+  User.findOne({ _id: userId })
+    .then((user) => {
+      if (user) {
+        const card = user.card || [];
+        console.log(card);
+        console.log(articleId);
+        const article = card.find(item => item.article == articleId);
+        if (!article) {
+          card.push({
+            article: articleId,
+            quantity: 1,
+          });
+        } else {
+          article.quantity += 1;
+        }
+        user.card = card;
+        return user.save();
+      } else {
+        throw new Error('huynya');
+      }
+    })
+    .then(() => res.send('added to card'))
+    .catch(next);
+});
+
+router.delete('/card', (req, res, next) => {
+  const articleId = req.body.id;
+  const userId = req.cookies.user;
+
+  User.updateOne(
+      { _id: userId },
+      // { $pull: { 'card.$.article': { $in: [ObjectId(articleId)] } } })
+      { $pull: { card : { article : ObjectId(articleId) } } },
+  )
+  .then(() => res.send('asd'))
+  .catch(next);
+});
+
+router.post('/card/clear', (req, res, next) => {
+  const userId = req.cookies.user;
+
+  User.updateOne({ _id: userId }, { card: [] })
+    .then(() => res.send('done'))
     .catch(next);
 });
 
