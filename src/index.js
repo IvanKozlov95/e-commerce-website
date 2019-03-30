b01a7eae4f952fc2296a875b251ed4bbf22a7bff
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-03-30 07:29:06 -0700

    Moved index

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express         = require('express');
+const createError     = require('http-errors');
+const cookieParser    = require('cookie-parser');
+const mongoose        = require('./lib/mongoose');
+
+require('./models');
+
+const userRouter      = require('./routes/user');
+const loginRouter     = require('./routes/login');
+const dashboardRouter = require('./routes/dashboard');
+
+const app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/../public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.url);
+
+  if (req.method === 'POST') {
+    console.log('Request params: ' + JSON.stringify(req.body));
+  }
+
+  next();
+});
+
+app.use('/users', userRouter);
+app.use('/login', loginRouter);
+app.use('/dashboard', dashboardRouter);
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  console.error(err);
+  res.status(err.status || 500);
+  res.send({ message: err.message });
+});
+
+app.listen(3000, () => console.log(`Listening on port: ${3000}`));
