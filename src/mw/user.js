2123e7aa3ebac3c33065edd3b26ed3fb2200aa3d
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-03-31 10:15:03 -0700

    Refactor

--- src/mw/user.js
+++ src/mw/user.js
@@ -1,12 +1,18 @@
 const mongoose        = require('mongoose');
 const User            = mongoose.model('User');
 
+const isAdmin = (user) => {
+  const name = user.name.toLowerCase();
+  return name === 'ivan' || name === 'Kevin';
+}
+
 function addUser(req, res, next) {
   if (req.cookies.user) {
     console.log('fetching user with id ' + req.cookies.user);
     User.findById(req.cookies.user)
       .populate('card.article')
       .then(user => req.user = user)
+      .then(user => req.user.isAdmin = isAdmin(req.user))
       .then(() => next())
       .catch(next);
   } else {
@@ -33,12 +39,7 @@ const userAnon = (req, res, next) => {
 
 const userAdmin = (req, res, next) => {
   const user = req.user;
-  const name = user.name.toLowerCase();
-  console.log(user);
-  if (name === 'ivan' || name === 'Kevin')
-    next();
-  else
-    res.redirect('/dashboard');
+  isAdmin(user) ? next() : res.redirect('/dashboard');
 };
 
 module.exports = { userLogged, userAnon, userAdmin, addUser };
