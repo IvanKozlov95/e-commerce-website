cffcb332d862a73b2188f3ba00076b88ae2d057f
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-03-31 10:45:11 -0700

    Only anon can register

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,12 +2,13 @@ const express     = require('express');
 const router      = express.Router();
 const mongoose    = require('mongoose');
 const User 	      = mongoose.model('User');
+const { userAnon, userLogged, userAdmin, addUser }  = require('../mw/user');
 
-router.get('/', (req, res, next) => {
+router.get('/', userAnon,  (req, res, next) => {
   res.render('login');
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', userAnon, (req, res, next) => {
   User.findOne({ name: req.body.name, password: req.body.password })
     .then((user) => {
         console.log(user);
