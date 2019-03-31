b95a2d8af89d19c1d01058292a48920faa9c0c10
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-03-30 22:10:01 -0700

    Refactor

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -6,15 +6,10 @@ const User            = mongoose.model('User');
 
 router.get('/', (req, res, next) => {
   User.findById(req.cookies.user)
-  .populate('card.article')
-  .then((user) => {
-    Article.find({})
-      .then(articles => {
-        console.log(user.card.map(console.log));
-        res.render('dashboard', { articles, user })
-      })
-      .catch(next);
-  });
+    .populate('card.article')
+    .then((user) => Article.find({}))
+    .then(articles => res.render('dashboard', { articles, user }))
+    .catch(next);
 });
 
 router.get('/admin', (req, res, next) => {
